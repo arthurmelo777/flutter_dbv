63c2b0bae516bb27961a257ab00aac6d2b58bde4
Arthur Melo <53615432+arthurmelo777@users.noreply.github.com>
2024-03-26 14:43:19 -0300

    Adição da tela 1

---
+++ primeira_tela.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+void main () {
+  runApp(MaterialApp(
+  home: MyApp(),));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    appBar: HeaderWidget(),
+    body: Center(child: Column(
+      children: [
+        Text('Descoberta espiritual',style: TextStyle(fontSize: 20)),
+        Text('Memorizar e demonstrar seu conhecimento: ', style: TextStyle(fontSize: 16)),
+        CheckboxListTile(
+        title: Text('Criação: O que Deus criou em cada dia da Criação.'),
+        value: true,
+        onChanged: (bool? value) {},
+        controlAffinity: ListTileControlAffinity.leading),
+        CheckboxListTile(
+        title: Text('10 Pragas: Quais as pragas que caíram sobre o Egito.'),
+        value: false,
+        onChanged: (bool? value) {},
+        controlAffinity: ListTileControlAffinity.leading),
+        CheckboxListTile(
+        title: Text('12 Tribos: O nome de cada uma das 12 tribos de Israel.'),
+        value: true,
+        onChanged: (bool? value) {},
+        controlAffinity: ListTileControlAffinity.leading),
+        CheckboxListTile(
+        title: Text('39 livros do Antigo Testamento e demonstrar habilidade para encontrar qualquer um deles.'),
+        value: true,
+        onChanged: (bool? value) {},
+        controlAffinity: ListTileControlAffinity.leading),
+      ])),
+      
+    bottomNavigationBar: FooterWidget(),
+    );
+  }
+}
+
+class HeaderWidget extends StatelessWidget implements PreferredSizeWidget {
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+  @override
+  Widget build(BuildContext context) {
+    return AppBar (
+    backgroundColor: Colors.blue,
+    title: Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      Icon(Icons.menu), // Foto dbv
+      Text(
+      'Amigo',
+      style: TextStyle(
+      fontSize: 18,
+      fontWeight: FontWeight.bold,
+      ),
+      ),
+      Icon(Icons.favorite), // Foto Amigo
+    ],
+    ),
+    );
+  }
+}
+
+class FooterWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+    color: Colors.blue,
+    child: Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      IconButton(
+      icon: Icon(Icons.home),
+      onPressed: () {},
+      ),
+      IconButton(
+      icon: Icon(Icons.person),
+      onPressed: () {},
+      ),
+    ],
+    ),
+    );
+  }
+}
